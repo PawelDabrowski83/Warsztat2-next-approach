697df6dfe920ebfcd06dee53489b56126c3ba3e0
Paweł Dąbrowski <fluorix@gmail.com>
2020-02-25 15:22:35 +0100

    user details

--- src/main/java/pl/coderslab/users/UserServiceConsole.java
+++ src/main/java/pl/coderslab/users/UserServiceConsole.java
@@ -31,12 +31,13 @@ public class UserServiceConsole {
                 switch (key) {
                     case "n":
                         System.out.println("Nowy użytkownik");
+                        User newUser = createUser(scanner);
+                        System.out.println("User created: " + newUser);
                         break;
                     case "x":
                         System.out.println("Exit");
                         return;
                     default:
-                        System.out.println("Sprawdzam czy wpisano prawidłowe id");
                         try {
                             userId = Integer.parseInt(key);
                             Optional<User> optionalUser = Optional.ofNullable(USER_DAO.read(userId));
@@ -55,6 +56,7 @@ public class UserServiceConsole {
         }
     }
     private static void userSettings(Scanner scanner, User user) {
+        System.out.println("Current user: " + user);
         System.out.println(USER_MENU);
         while(scanner.hasNextLine()) {
             String key = scanner.nextLine().trim().toLowerCase();
@@ -62,21 +64,75 @@ public class UserServiceConsole {
             switch (key) {
                 case "n":
                     System.out.println("stwórz nowego");
+                    User newUser = createUser(scanner);
+                    System.out.println("User created: " + newUser);
+                    System.out.println(USER_MENU);
                     break;
                 case "x":
                     System.out.println("Exit");
                     return;
                 case "d":
                     System.out.println("Delete");
-                    break;
+                    user = deleteUser(user);
+                    System.out.println("User deleted");
+                    System.out.println(USER_MENU);
+                    return;
                 case "e":
                     System.out.println("Edycja");
+                    user = editUser(scanner, user);
+                    System.out.println("User edited: " + user);
+                    System.out.println(USER_MENU);
                     break;
                 default:
                     System.out.println(USER_MENU);
             }
             return;
         }
+    }
+
+    private static User createUser(Scanner scanner) {
+        String[] messages = {"Podaj nazwę użytkownika:", "Email:", "Hasło:"};
+        String[] buffer = new String[messages.length];
+        int counter = 0;
+        while(counter < messages.length) {
+            System.out.println(messages[counter]);
+            buffer[counter] = scanner.nextLine().trim();
+            counter++;
+        }
+        System.out.println("Tworzę nowego użytkownika");
+        User user = new User(buffer[0], buffer[1], buffer[2]);
+        user = USER_DAO.create(user);
+        return user;
+    }
+
+    private static User editUser(Scanner scanner, User user) {
+        String[] messages = {"Podaj nazwę użytkownika: ", "Email: ", "Hasło: "};
+        String[] buffer = new String[messages.length];
+        buffer[0] = user.getName();
+        buffer[1] = user.getEmail();
+        buffer[2] = user.getPassword();
+        int counter = 0;
+        while(counter < messages.length) {
+            System.out.println("Wpisz nowe dane lub wciśnij ENTER, aby zostawić bez zmian");
+            System.out.println(messages[counter] + buffer[counter]);
+            String key = scanner.nextLine().trim();
+            if(!"".equals(key)) {
+                buffer[counter] = key;
+            }
+            counter++;
+        }
+        System.out.println("Edytuję użytkownika");
+        user.setName(buffer[0]);
+        user.setEmail(buffer[1]);
+        user.setPassword(buffer[2]);
+        USER_DAO.update(user);
+        return user;
+    }
+
+    private static User deleteUser(User user) {
+        USER_DAO.delete(user.getId());
+        return new User();
+
     }
 
 
